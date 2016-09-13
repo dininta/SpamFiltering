88ac289b2a09cb508616011ecbf79a114c73655c
dininta <dinintaannisa@yahoo.com>
2016-09-13 16:03:40 +0700

    add classify function in cart

--- Spam Filtering/src/spam/filtering/Cart.java
+++ Spam Filtering/src/spam/filtering/Cart.java
@@ -11,7 +11,6 @@ import weka.core.DenseInstance;
 import weka.classifiers.trees.SimpleCart;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 
@@ -22,11 +21,12 @@ import java.util.Arrays;
 public class Cart {
     
     protected SimpleCart tree;
+    protected Instances data;
     
     public Cart(String arffFile) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(arffFile));
-            Instances data = new Instances(reader);
+            data = new Instances(reader);
             reader.close();
             data.setClassIndex(data.numAttributes() - 1);
 
@@ -40,6 +40,17 @@ public class Cart {
     
     public void printTree() {
         System.out.println(tree.toString());
+    }
+    
+    public void classify(String[] attribute) throws Exception {
+        Instance newInstance = new DenseInstance(data.numAttributes());
+        newInstance.setDataset(data);
+        for (int i = 0; i < data.numAttributes()-1; i++) {
+            newInstance.setValue(i, attribute[i]);
+        }
+        
+        double[] result = tree.distributionForInstance(newInstance);
+        System.out.println("Class probabilities: " + Arrays.toString(result));
     }
     
 }
