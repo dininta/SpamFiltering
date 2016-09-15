b2823a7001d1e9de8f4eda68f8aa1ee0b0d4df57
dininta <dinintaannisa@yahoo.com>
2016-09-15 08:39:48 +0700

    move code to SpamFiltering

--- Spam Filtering/src/spam/filtering/SpamFiltering.java
+++ Spam Filtering/src/spam/filtering/SpamFiltering.java
@@ -5,17 +5,47 @@
  */
 package spam.filtering;
 
+import IndonesianNLP.IndonesianSentenceFormalization;
+import IndonesianNLP.IndonesianStemmer;
+import java.util.ArrayList;
+
 /**
  *
  * @author mochamadtry
  */
 public class SpamFiltering {
 
-    /**
-     * @param args the command line arguments
-     */
-//    public static void main(String[] args) {
-//    
-//    }
+    protected IndonesianSentenceFormalization formalizer;
+    protected IndonesianStemmer stemmer;
+    
+    public SpamFiltering() {
+        formalizer = new IndonesianSentenceFormalization();
+        stemmer = new IndonesianStemmer();
+    }
+    
+    public ArrayList preProcess(ArrayList listOfText) {
+        ArrayList processed = new ArrayList();
+        for (int i = 0; i < listOfText.size(); i++) {
+            // Formalization
+            String text = formalizer.normalizeSentence(listOfText.get(i).toString());
+            
+            //Replace All Non Alphabetic
+            text = text.replaceAll("[^a-zA-Z\\s]", " ");
+            
+            //Stop Word
+            formalizer.initStopword();
+            text = formalizer.deleteStopword(text);
+                  
+            // Stemming
+            StringBuilder result = new StringBuilder();
+            String[] words = text.split(" ");
+            for (String word : words) {
+                if (word.length() > 1) result.append(stemmer.stem(word + " "));
+            }
+            
+            processed.add(result.toString());
+        }
+        return processed;
+    }
     
 }
--- Spam Filtering/src/spam/filtering/main.java
+++ Spam Filtering/src/spam/filtering/main.java
@@ -6,8 +6,6 @@
 package spam.filtering;
 
 import java.util.ArrayList;
-import IndonesianNLP.IndonesianSentenceFormalization;
-import IndonesianNLP.IndonesianStemmer;
 
 /**
  *
@@ -18,58 +16,11 @@ public class main {
         FileReaderCSV reader = new FileReaderCSV();
         ArrayList spam = reader.readFile("traindata/spam.csv");
         ArrayList notSpam = reader.readFile("traindata/notSpam.csv");
-
-        System.out.println("BEFORE");
-        
-        System.out.println(spam.get(7));
         
         // Preprocess
-        IndonesianSentenceFormalization formalizer = new IndonesianSentenceFormalization();
-        IndonesianStemmer stemmer = new IndonesianStemmer();
-        
-        for (int i = 0; i < spam.size(); i++) {
-            // Formalization
-            String text = formalizer.normalizeSentence(spam.get(i).toString());
-            
-            //Replace All Non Alphabetic
-            text = text.replaceAll("[^a-zA-Z\\s]", " ");
-            
-            //Stop Word
-            formalizer.initStopword();
-            text = formalizer.deleteStopword(text);
-            
-            // Stemming
-            StringBuilder result = new StringBuilder();
-            String[] words = text.split(" ");
-            for (String word : words) {
-                if (word.length() > 1) result.append(stemmer.stem(word + " "));
-            }
-            
-            spam.set(i, result.toString());
-        }
-        for (int i = 0; i < notSpam.size(); i++) {
-            // Formalization
-            String text = formalizer.normalizeSentence(notSpam.get(i).toString());
-            
-            //Replace All Non Alphabetic
-            text = text.replaceAll("[^a-zA-Z\\s]", " ");
-            
-            //Stop Word
-            formalizer.initStopword();
-            text = formalizer.deleteStopword(text);
-                  
-            // Stemming
-            StringBuilder result = new StringBuilder();
-            String[] words = text.split(" ");
-            for (String word : words) {
-                if (word.length() > 1) result.append(stemmer.stem(word + " "));
-            }
-            
-            notSpam.set(i, result.toString());
-        }
-        
-        System.out.println("AFTER");
-        System.out.println(spam.get(7));
+        SpamFiltering spamFiltering = new SpamFiltering();
+        spam = spamFiltering.preProcess(spam);
+        notSpam = spamFiltering.preProcess(notSpam);
         
         reader.writeFile(spam, "PreprocessSpam.csv");
         reader.writeFile(notSpam, "PreprocessNotSpam.csv");
